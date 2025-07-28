f508d3b0113de6f93b45797059afa0f2ae282bfe
HBSLDT <hydraongm@gmail.com>
2025-07-28 22:58:56 +0100

    Create products.js

---
+++ products.js
@@ -0,0 +1,42 @@
+// Product database for HydraAI
+
+const products = {
+  glovesUp: {
+    freeTaster: {
+      name: "Free Taster Pad Session",
+      price: "£0",
+      link: "https://wa.me/447825954329?text=Hi%20I%20want%20a%20free%20Gloves%20Up%20taster"
+    },
+    paidSession: {
+      name: "1:1 Boxing Session",
+      price: "£30",
+      link: "https://wa.me/447825954329?text=Hi%20I%20want%20a%201:1%20session%20at%20Gloves%20Up"
+    }
+  },
+  noExcuseFitness: {
+    challenge: {
+      name: "7-Day Boxer Beast Challenge",
+      price: "£7",
+      link: "https://noexcusefit.gumroad.com/l/hwtuj"
+    },
+    competePlan: {
+      name: "6-Week Complete Compete Fighters Plan",
+      price: "£30",
+      link: "https://noexcusefit.gumroad.com/l/lauiq"
+    }
+  },
+  salesUp: {
+    starterPack: {
+      name: "Sales Up Starter Pack",
+      price: "Free",
+      link: "https://noexcusefit.gumroad.com/l/tizji"
+    },
+    masterGuide: {
+      name: "Sales Up Master Guide",
+      price: "£20",
+      link: "https://noexcusefit.gumroad.com/l/wownvr"
+    }
+  }
+};
+
+// You can reference this in hydraai.html for dynamic response upgrades later
